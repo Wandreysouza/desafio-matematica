a3eaee2e1acc52b5fafa20544f9038230e694b19
tiov4d3r <emanuel.filho08@gmail.com>
2021-09-17 23:51:37 -0300

    a função timeit agora faz exatamente o que o nome diz, quantifica o tempo de espera da função.

--- src/timeit.py
+++ src/timeit.py
@@ -4,28 +4,34 @@ import time
 def timeit(method):
     def timed(*args, **kw):
         ts = time.time()
-        kw = method(*args, **kw)
-        te = time.time()
-        name = ' '.join(kw['name'].split('_'))
-
-        header = 'BENCHMARK'
-        metodo = f'METODO: {name.capitalize()}'
-        t = f'{tt:.3f}s' if (tt := te - ts) > 1 else f'{tt*1000:.3f}ms'
-        tempo = f'TEMPO: {t}'
-        sucessos = f'    - PASSED: {kw["sucess"]}'
-        falhas = f'    - FAILED: {kw["fail"]}'
-        inputs_falhados = f'INPUTS QUE FALHARAM: {", ".join(map(str, kw["fails"]))}' if kw[
-            'fail'] else ''
-
-        texto = [metodo, tempo, sucessos, falhas] + \
-            ([inputs_falhados] if inputs_falhados else [])
-        i = max(map(len, texto)) + 20
-
-        txt = f'{header:=^{i}}\n'
-        txt += '\n'.join(f'||  {t:<{i-6}}' + '||' for t in texto)
-        txt += f'\n{"":=^{i}}'
-
-        print(txt)
-
-        return kw
+        return time.time() - ts
     return timed
+
+# def timeit(method):
+#     def timed(*args, **kw):
+#         ts = time.time()
+#         kw = method(*args, **kw)
+#         te = time.time()
+#         name = ' '.join(kw['name'].split('_'))
+
+#         header = 'BENCHMARK'
+#         metodo = f'METODO: {name.capitalize()}'
+#         t = f'{tt:.3f}s' if (tt := te - ts) > 1 else f'{tt*1000:.3f}ms'
+#         tempo = f'TEMPO: {t}'
+#         sucessos = f'    - PASSED: {kw["sucess"]}'
+#         falhas = f'    - FAILED: {kw["fail"]}'
+#         inputs_falhados = f'INPUTS QUE FALHARAM: {", ".join(map(str, kw["fails"]))}' if kw[
+#             'fail'] else ''
+
+#         texto = [metodo, tempo, sucessos, falhas] + \
+#             ([inputs_falhados] if inputs_falhados else [])
+#         i = max(map(len, texto)) + 20
+
+#         txt = f'{header:=^{i}}\n'
+#         txt += '\n'.join(f'||  {t:<{i-6}}' + '||' for t in texto)
+#         txt += f'\n{"":=^{i}}'
+
+#         print(txt)
+
+#         return kw
+#     return timed
