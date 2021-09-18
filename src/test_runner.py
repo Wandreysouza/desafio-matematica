00ef27f5a6ee3ff735f972245fee7cd3d7feb78d
tiov4d3r <emanuel.filho08@gmail.com>
2021-09-18 00:13:05 -0300

    timeit inserido diretamente dentro de test_runner.py

--- src/test_runner.py
+++ src/test_runner.py
@@ -1,24 +1,50 @@
-from timeit import timeit
+import time
 from terno_pitagorico import funcs
 
 inputs = [12, 90, 108, 840, 1000, 1001, 30000]
 outputs = [funcs[-1](i) for i in inputs]
 
 
-@timeit
-def run_tests(func, **kw):
-    sucess = 0
+def timeit(method, *args, **kwargs):
+    ts = time.time()
+    method(*args)
+    return time.time() - ts
+
+
+def __get_table(**kw):
+    name = ' '.join(kw['NOME'].split('_'))
+    header = 'BENCHMARK'
+    metodo = f'METODO: {name.title()}'
+    t = f'{kw["TEMPO"]:.3f}s' if kw['TEMPO'] > 1 else f'{kw["TEMPO"]*1000:.3f}ms'
+    tempo = f'TEMPO: {t}'
+    sucessos = f'    - PASSED: {kw["SUCESSO"]}'
+    inputs_falhados = f'INPUTS QUE FALHARAM: {", ".join(map(str, kw["ERROS"]))}' if kw[
+        'FALHA'] else ''
+    inputs_falhados = ('{%s}' % inputs_falhados) if inputs_falhados else ''
+    falhas = f'    - FAILED: {kw["FALHA"]}{inputs_falhados}'
+
+    texto = [metodo, tempo, sucessos, falhas] + \
+        ([inputs_falhados] if inputs_falhados else [])
+    i = max(map(len, texto)) + 20
+
+    txt = f'{header:=^{i}}\n'
+    txt += '\n'.join(f'||  {t:<{i-6}}' + '||' for t in texto)
+    txt += f'\n{"":=^{i}}'
+
+    print(txt)
+
+
+def run_test(func):
+    sucesso = 0
     fails = []
-    for in_, out in zip(inputs, outputs):
-        try:
-            sucess += (flag := sorted(out) == sorted(func(in_)))
-            assert flag
-        except Exception:
-            fails.append(in_)
-    kw['sucess'] = sucess
-    kw['fail'] = len(fails)
-    kw['fails'] = fails.copy()
-    return kw
+    tempo_total = 0
+    for i, o in zip(inputs, outputs):
+        tempo_total += timeit(func, i)
+        flag = sorted(func(i)) == sorted(o)
+        sucesso += flag
+        if not flag:
+            fails.append(i)
+    return __get_table(NOME=func.__name__, TEMPO=tempo_total, SUCESSO=sucesso, FALHA=len(inputs)-sucesso, ERROS=fails)
 
 
 if __name__ == '__main__':
@@ -27,4 +53,4 @@ if __name__ == '__main__':
     print(f'{txt:^20}  ||')
     print('=' * 25 + '\n\n')
 
-    [run_tests(func, name=func.__name__) for func in funcs]
+    [run_test(func) for func in funcs]
